7e4f9324db97326b4985d1f9de9c066e51b2f6f1
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-02-04 23:27:54 +0100

    add index page with jsoon data displayed

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, price, tagline, city, country } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,11 +7,21 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", name) 
         const h2 = document.createElement( 'h2' );
+        const pricePhotographer = document.createElement( 'p' );
+        const taglinePhotographer = document.createElement( 'p' );
+        const locationPhotographer = document.createElement( 'p' );
+        locationPhotographer.textContent = `${city}, ${country}`;
         h2.textContent = name;
+        pricePhotographer.textContent = (`${price}€/jour`) ;
+        taglinePhotographer.textContent = tagline;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(taglinePhotographer);
+        article.appendChild(pricePhotographer);
+        article.appendChild(locationPhotographer);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
