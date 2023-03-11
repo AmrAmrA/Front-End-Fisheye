0fe455ad165a052a67f9f0a0ffeaa2c4555e46b7
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-03-11 14:10:29 +0100

    add sortings options

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,6 +1,7 @@
+let emptyArray        = [];
 class mediaFactory {
   constructor(data) {
-    const { title, image, video, likes, id, photographerId, artist } = data;
+    const { title, image, video, likes, id, photographerId, artist, date } = data;
     this._title           = title;
     this._artist          = artist;
     this._picture         = image
@@ -10,17 +11,22 @@ class mediaFactory {
     this._id              = id;
     this._photographerId  = photographerId;
     this.video            = video;
+    this.date             = date;
+    emptyArray.push(data);
+    console.log(emptyArray.sort((a, b) => a.title > b.title));
+    console.log(emptyArray.sort((a, b) => a.likes < b.likes));
+    console.log(emptyArray.sort((a, b) => a.date < b.date));
   }
 
   getuserGalleryCard() {
-    
+
     if(this._picture.includes('mp4')){this._picture = this._picture.replace("mp4", "png");}
 
     const article = document.createElement("article");
     article.setAttribute("tabindex", "0");
+    article.setAttribute("title", `${this._title}`);
     article.innerHTML = `
-    <div class="media__container">
-      <img src="${this._picture}" alt="${this._title} by ${this._artist}" class="artist__media" title="${this._title}">
+      <img src="${this._picture}" alt="${this._title} by ${this._artist}" class="artist__media" title="${this._title}" loading ="lazy">
       <header> 
       <h2 class="media__title">${this._title}</h2>
       <div class="media__likes">
@@ -28,7 +34,6 @@ class mediaFactory {
       <i class="fas fa-heart heart__media"></i>
       </div>
       </header>
-    </div>
       `
     return article;
   }
--- scripts/utils/slider.js
+++ scripts/utils/slider.js
@@ -1,71 +1,20 @@
-function openTheSlider() {
-  sliderBlock.style.display = "block";
-  mediaSection.style.display = "none";
-  photographersSection.style.display = "none";
-  pageHeader.style.display = "none";
-  const dropDown = document.querySelector("#photo__sort");
-  dropDown.style.display = "none";
-  labelText.style.display = "none";
-}
-function closeTheSlider() {
-  sliderBlock.style.display = "none";
-  mediaSection.style.display = "flex";
-  photographersSection.style.display = "block";
-  pageHeader.style.display = "flex";
-  const dropDown = document.querySelector("#photo__sort");
-  dropDown.style.display = "inline";
-  labelText.style.display = "inline";
-}
+const centralImage = document.querySelector(".central__image");
 
 
 
 
-
-
-
-// A value to lock when i Decrement or increment 
-// the number of likes
-let addHeart = true;
 
 function displaySlider() {
+  const rightArrow = document.querySelector(".right__direction");
+  const leftArrow = document.querySelector(".left__direction");
   const allMedia = document.querySelectorAll(".artist__media");
-  const closeSlider = document.querySelector(".close__slider");
-  let heartMedia = document.querySelectorAll(".heart__media");
-  let likesNumberMedia = document.querySelectorAll(".likesNumber");
-
-  for (let i = 0; i < heartMedia.length; i++) {
-    heartMedia[i].addEventListener("click", () => {
-      if (addHeart) {
-        likesNumberMedia[i].textContent++;
-        console.log(likesNumberMedia[i].textContent);
-        totalOfLikes.textContent++;
-        addHeart = false;
-      } else {
-        likesNumberMedia[i].textContent--;
-        totalOfLikes.textContent--;
-        addHeart = true;
-      }
-    });
-  }
-
-
-  closeSlider.addEventListener("click", closeTheSlider);
-  body.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" || e.key === "Enter") {
-      closeTheSlider();
-    }
-  });
+  
 
   for (let oneMedia of allMedia) {
     const mediaArray = Array.from(allMedia);
-    optionTitle.addEventListener("click", () => {});
     oneMedia.addEventListener("click", (e) => {
-      const centralImage = document.querySelector(".central__image");
-      const rightArrow = document.querySelector(".right__direction");
-      const leftArrow = document.querySelector(".left__direction");
 
       centralImage.src = e.target.src;
-      console.log(e.target.src);
       e.target.classList.remove("photo");
       let mediaIndex = mediaArray.indexOf(e.target);
       rightArrow.addEventListener("click", nextSlide);
@@ -107,3 +56,37 @@ function displaySlider() {
 }
 
 setTimeout(displaySlider, 1000);
+
+
+
+
+function openTheSlider() {
+  sliderBlock.style.display = "block";
+  mediaSection.style.display = "none";
+  photographersSection.style.display = "none";
+  pageHeader.style.display = "none";
+  const dropDown = document.querySelector("#photo__sort");
+  dropDown.style.display = "none";
+  labelText.style.display = "none";
+
+  const closeSlider = document.querySelector(".close__slider");
+  closeSlider.addEventListener("click", closeTheSlider);
+  body.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" || e.key === "Enter") {
+      closeTheSlider();
+    }
+  });
+
+}
+
+
+function closeTheSlider() {
+  sliderBlock.style.display = "none";
+  mediaSection.style.display = "flex";
+  photographersSection.style.display = "block";
+  pageHeader.style.display = "flex";
+  const dropDown = document.querySelector("#photo__sort");
+  dropDown.style.display = "inline";
+  labelText.style.display = "inline";
+}
+setTimeout(closeTheSlider, 1000);
