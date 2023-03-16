aeaa5b2e7456d6cca2b8a6256bde7541bb5a940f
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-03-16 11:15:56 +0100

    Refacto code for loops in medias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,10 @@
 let arraytitles = [];
 let arrayDates = [];
 let arrayLikes = [];
+const arraySort = []
+
+// Ton tableau initial
+// un tableau appelé par exemple arraySort <-- C'est celui ci que tu vas mettre à jour avec ton tri
 
 async function displayDataAndMedia() {
   const response = await fetch("data/photographers.json");
@@ -15,66 +19,118 @@ async function displayDataAndMedia() {
 displayDataAndMedia().then((data) => {
   // Display the photographer's card header
   {
-    for (const photographPage of data.photographers) {
-      if (photographPage.id == PhotographerId) {
-        const photographeModel = new photographerFactory(
-          photographPage,
-          "onePhotographer"
-        );
-        const userCardDOM = photographeModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-        moneyDay.textContent = `${photographPage.price}€/ jour`;
-      }
+    // Tu peux utiliser un find
+    const photographer = data.photographers.find(photographer => photographer.id == PhotographerId)
+    if(photographer) {
+      const photographeModel = new photographerFactory(
+        photographer,
+        "onePhotographer"
+      );
+      const userCardDOM = photographeModel.getUserCardDOM();
+      photographersSection.appendChild(userCardDOM);
+      moneyDay.textContent = `${photographer.price}€/ jour`;
     }
+  
   }
   // Display the photographer's gallery media
-  for (let media of data.media) {
-    if (media.photographerId == PhotographerId) {
-      const mediaModel = new mediaFactory(media);
-      const mediaCardDOM = mediaModel.getuserGalleryCard();
-      mediaSection.appendChild(mediaCardDOM);
+  const medias = data.media.filter(media => media.photographerId == PhotographerId)
+  for (const media of medias) {
+    const mediaModel = new mediaFactory(media);
+    const mediaCardDOM = mediaModel.getuserGalleryCard();
+    mediaSection.appendChild(mediaCardDOM);
 
-      // gathering the media by alphabetical order in an array
-      arraytitles.push(mediaCardDOM);
-      arraytitles.sort((a, b) => a.title > b.title);
-
-      // Display the media by alphabetical order in the dom
-      optionTitle.addEventListener("click", () => {
-        for (let i = 0; i < arraytitles.length; i++) {
-          mediaSection.appendChild(arraytitles[i]);
-        }
-      });
-      // gathering the media by date in an array
-      arrayDates.push(mediaCardDOM);
-      // Display the media by date in the dom
-      optionDate.addEventListener("click", () => {
-        for (let i = 0; i < arrayDates.length; i++) {
-          parseInt(arrayDates[i].getAttribute("date"));
-          arrayDates.sort( (a, b) => parseInt(a.getAttribute("date")) > parseInt(b.getAttribute("date")) );
-          mediaSection.appendChild(arrayDates[i]);
-        }
-      });
-
-      // gathering the media by likes in an array
-      arrayLikes.push(mediaCardDOM);
-      // Display the media by likes in the dom
-      optionLikes.addEventListener("click", () => {
-        for (let i = 0; i < arrayLikes.length; i++) {
-          parseInt(arrayLikes[i].getAttribute("likes"));
-          arrayLikes.sort(
-            (a, b) =>
-              parseInt(a.getAttribute("likes")) <
-              parseInt(b.getAttribute("likes"))
-          );
-          mediaSection.appendChild(arrayLikes[i]);
-        }
-      });
-    }
     const AllLikes = document.querySelectorAll(".likesNumber");
     let zero = 0;
-    for (let i = 0; i < AllLikes.length; i++) {
-      let total = (zero += parseInt(AllLikes[i].textContent));
+    for (const allLike of AllLikes) {
+      const total = (zero += parseInt(allLike.textContent));
       totalOfLikes.textContent = `${total}`;
     }
   }
+
+  const select = document.querySelector("#photo__sort");
+  select.addEventListener("change", (event) => {
+    const value = event.target.value;
+    arraySort.push(...medias)
+    switch (value) {
+      case "title":
+        arraySort.sort((a, b) => a.title > b.title);
+        break;
+      case "date":
+        arraySort.sort((a, b) => parseInt(a.date) > parseInt(b.date));
+        break;
+      case "likes":
+        arraySort.sort((a, b) => parseInt(a.likes) > parseInt(b.likes));
+        break;
+    }
+    // Tu mets à jour ton DOM
+    mediaSection.innerHTML = "";
+    for (const element of arraySort) {
+      mediaSection.appendChild(element);
+    }
+  });
+
+  // for (const media of data.media) {
+  //   if (media.photographerId == PhotographerId) {
+  //     const mediaModel = new mediaFactory(media);
+  //     const mediaCardDOM = mediaModel.getuserGalleryCard();
+  //     mediaSection.appendChild(mediaCardDOM);
+
+  //     // // gathering the media by alphabetical order in an array
+  //     // arraytitles.push(mediaCardDOM);
+  //     // arraytitles.sort((a, b) => a.title > b.title);
+
+  //     // // Display the media by alphabetical order in the dom
+  //     // optionTitle.addEventListener("click", () => {
+  //     //   for (let i = 0; i < arraytitles.length; i++) {
+  //     //     mediaSection.appendChild(arraytitles[i]);
+  //     //   }
+  //     // });
+  //     // // gathering the media by date in an array
+  //     // arrayDates.push(mediaCardDOM);
+  //     // // Display the media by date in the dom
+  //     // optionDate.addEventListener("click", () => {
+  //     //   for (let i = 0; i < arrayDates.length; i++) {
+  //     //     parseInt(arrayDates[i].getAttribute("date"));
+  //     //     arrayDates.sort( (a, b) => parseInt(a.getAttribute("date")) > parseInt(b.getAttribute("date")) );
+  //     //     mediaSection.appendChild(arrayDates[i]);
+  //     //   }
+  //     // });
+
+  //     // // gathering the media by likes in an array
+  //     // arrayLikes.push(mediaCardDOM);
+  //     // // Display the media by likes in the dom
+  //     // optionLikes.addEventListener("click", () => {
+  //     //   for (let i = 0; i < arrayLikes.length; i++) {
+  //     //     parseInt(arrayLikes[i].getAttribute("likes"));
+  //     //     arrayLikes.sort(
+  //     //       (a, b) =>
+  //     //         parseInt(a.getAttribute("likes")) <
+  //     //         parseInt(b.getAttribute("likes"))
+  //     //     );
+  //     //     mediaSection.appendChild(arrayLikes[i]);
+  //     //   }
+  //     // });
+  //   }
+  //   const AllLikes = document.querySelectorAll(".likesNumber");
+  //   let zero = 0;
+  //   for (let i = 0; i < AllLikes.length; i++) {
+  //     let total = (zero += parseInt(AllLikes[i].textContent));
+  //     totalOfLikes.textContent = `${total}`;
+  //   }
+  // }
 });
+
+
+/*
+  Pour le tri :
+    - Tu dois t'ancrer sur ton select
+    - Tu y ajoutes un event listener sur le changement de valeur
+    - Tu récupères la valeur du select
+
+    Avec cette valeur, tu sais quelle opération de tri tu dois faire
+    Tu effectues le tri et tu mets à jour ton tableau (du coup tu n'a qu'un seul tableau, arraySort)
+
+    Par exemple pour l'approche :
+     - Tu fais un switch pour faire le tri
+     - en dehors du switch, après le switch, tu mets à jour tes medias dans le DOM
+*/
--- scripts/utils/slider.js
+++ scripts/utils/slider.js
@@ -1,89 +1,92 @@
-let un = 1;
 function displaySlider() {
-  let heartMedia = document.querySelectorAll(".heart__media");
-  let likesNumberMedia = document.querySelectorAll(".likesNumber");
-
-  // for (let i = 0; i < heartMedia.length; i++) {
-  //   heartMedia[i].addEventListener("click", () => {
-  //     if (heartMedia[i].classList.toggle("liked")) {
-  //       likesNumberMedia[i].textContent++;
-  //       totalOfLikes.textContent++;
-  //       console.log(heartMedia[i].classList);
-  //       heartMedia[i].classList.remove("liked");
-  //     } else {
-  //       likesNumberMedia[i].textContent--;
-  //       totalOfLikes.textContent--;
-  //       console.log(heartMedia[i].classList);
-  //     }
-  //   });
-  // }
-
-  heartMedia.forEach((heart, index) => {
-    heart.addEventListener("click", (e) => {
-      if (e.target.nearestViewportElement.classList.contains("heart__toggle") && un == 1 ) {
-        likesNumberMedia[index].textContent++;
-        totalOfLikes.textContent++;
-        un++;
-      } else {
-        likesNumberMedia[index].textContent--;
-        totalOfLikes.textContent--;
-        un--;
-      }
-    });
-  });
-
-
-
-
-  const rightArrow = document.querySelector(".right__direction");
-  const leftArrow = document.querySelector(".left__direction");
-  const allMedia = document.querySelectorAll(".artist__media");
+// function to increase or decrease the number of likes
+likesContent();
+// function to move forward or backward on the number of slides 
+const { allMedia, rightArrow, leftArrow } = slidesContent();
 
   for (let oneMedia of allMedia) {
     const mediaArray = Array.from(allMedia);
-    oneMedia.addEventListener("click", (e) => {
-      centralImage.src = e.target.src;
-      e.target.classList.remove("photo");
-      let mediaIndex = mediaArray.indexOf(e.target);
-      rightArrow.addEventListener("click", nextSlide);
-      leftArrow.addEventListener("click", previousSlide);
-      openTheSlider();
+    oneMedia.addEventListener("click", function (e) {
+        centralImage.src = e.target.src;
+        e.target.classList.remove("photo");
+        let mediaIndex = mediaArray.indexOf(e.target);
+        rightArrow.addEventListener("click", nextSlide);
+        leftArrow.addEventListener("click", previousSlide);
+        openTheSlider();
 
-      function nextSlide() {
-        if (mediaIndex < mediaArray.length - 1) {
-          centralImage.src = mediaArray[mediaIndex + 1].src;
-          mediaIndex++;
-        } else {
-          centralImage.src = mediaArray[0].src;
-          mediaIndex = 0;
+        function nextSlide() {
+          if (mediaIndex < mediaArray.length - 1) {
+            centralImage.src = mediaArray[mediaIndex + 1].src;
+            mediaIndex++;
+          } else {
+            centralImage.src = mediaArray[0].src;
+            mediaIndex = 0;
+          }
         }
-      }
 
-      function previousSlide() {
-        if (mediaIndex > 0) {
-          centralImage.src = mediaArray[mediaIndex - 1].src;
-          mediaIndex--;
-        } else {
-          centralImage.src = mediaArray[mediaArray.length - 1].src;
-          mediaIndex = mediaArray.length - 1;
+        function previousSlide() {
+          if (mediaIndex > 0) {
+            centralImage.src = mediaArray[mediaIndex - 1].src;
+            mediaIndex--;
+          } else {
+            centralImage.src = mediaArray[mediaArray.length - 1].src;
+            mediaIndex = mediaArray.length - 1;
+          }
         }
-      }
 
-      function arrowClick(event) {
-        if (event.keyCode == 39) {
-          nextSlide();
-          return;
-        } else if (event.keyCode == 37) {
-          previousSlide();
+        function arrowClick(event) {
+          if (event.keyCode == 39) {
+            nextSlide();
+            return;
+          } else if (event.keyCode == 37) {
+            previousSlide();
+          }
         }
-      }
 
-      body.addEventListener("keydown", arrowClick);
-    });
+        body.addEventListener("keydown", arrowClick);
+      });
+  }
+
+  function slidesContent() {
+    const rightArrow = document.querySelector(".right__direction");
+    const leftArrow = document.querySelector(".left__direction");
+    const allMedia = document.querySelectorAll(".artist__media");
+    return { allMedia, rightArrow, leftArrow };
   }
 }
 
 setTimeout(displaySlider, 1000);
+
+
+
+
+
+
+
+
+
+function likesContent() {
+  let heartMedia = document.querySelectorAll(".heart__media");
+  let likesNumberMedia = document.querySelectorAll(".likesNumber");
+  numberLikes(heartMedia, likesNumberMedia);
+}
+
+function numberLikes(heartMedia, likesNumberMedia) {
+  for (let i = 0; i < heartMedia.length; i++) {
+    heartMedia[i].addEventListener("click", () => {
+      if (heartMedia[i].classList.contains("heart__toggle")) {
+        likesNumberMedia[i].textContent++;
+        totalOfLikes.textContent++;
+        heartMedia[i].classList.remove("heart__toggle");
+        console.log(heartMedia[i].classList);
+      } else {
+        likesNumberMedia[i].textContent--;
+        totalOfLikes.textContent--;
+        console.log(heartMedia[i].classList);
+      }
+    });
+  }
+}
 
 function openTheSlider() {
   sliderBlock.style.display = "block";
